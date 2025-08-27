3884ef5c28c0b12cfa56cfc9f998ab99163242ac
janaghoniem <jana2208912@miuegypt.edu.eg>
2025-08-27 23:47:58 +0300

    Performance section

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -66,27 +66,27 @@ function App() {
               </tbody>
             </table>
           </section>
-          <section id='Performance'>
+          <section id='Performance' className='m-15'>
             <h2 className='section-title'>Performance</h2>
             <div className='performance-content'>
-              <div>
+              <div className='flex flex-col items-center'>
                 <img src="/solution-performance.svg" alt="" />
-                <div className='section-text'>
+                <div className='section-text max-w-[550px] text-center'>
                   <h4>The Solution</h4>
                   <p>We addressed this by balancing the dataset and training our models to perform better on a wider variety of fruits.</p>
                 </div>
               </div>
-              <div className='flex justify-between items-center'>
-                <div>
+              <div className='flex justify-between items-center -mt-50'>
+                <div className='flex flex-col items-start'>
                   <img src="/challenge-performance.svg" alt="" />
-                  <div className='section-text'>
+                  <div className='section-text max-w-[550px] text-left'>
                     <h4>The Challenge</h4>
                     <p>The original dataset had a severe class imbalance, with one class ("Apple") having thousands more images than others. This caused bias.</p>                  
                     </div>
                 </div>
-                <div>
+                <div className='flex flex-col items-end'>
                   <img src="/results-performance.svg" alt="" />
-                  <div className='section-text'>
+                  <div className='section-text max-w-[550px] text-right'>
                     <h4>The Results</h4>
                     <p>After extensive testing, EfficientNetB0 with our custom augmentations achieved the highest scores, with an accuracy of 96% </p>
                   </div>
